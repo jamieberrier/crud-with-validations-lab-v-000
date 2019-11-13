79047dab7e3012688c9df34d136619eeeff42299
jamieberrier <berrier.jamie@gmail.com>
2019-11-13 17:51:50 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -13,23 +13,8 @@ class Song < ApplicationRecord
 
   def same_artist_and_year?
     # does song title exist by same artist, if so check if year matches
-    binding.pry
+    #binding.pry
     Song.find_by(title: self.title, artist_name: self.artist_name, year: self.year)
-
-=begin
-    Song.all.each do |song|
-      if song.title == self.title
-        if song.artist_name == self.artist_name
-          song.year == self.year
-          return true
-        else
-          return false
-        end
-      else
-        return false
-      end
-    end
-=end
   end
 
   def released?
