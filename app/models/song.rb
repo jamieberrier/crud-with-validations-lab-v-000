3e2c0c11a28071293d2f24962a0af46a4983e9e9
jamieberrier <berrier.jamie@gmail.com>
2019-11-13 03:31:50 +0000

    Automatically backed up by Learn

---
+++ app/models/song.rb
@@ -0,0 +1,10 @@
+class Song < ApplicationRecord
+  # Cannot be repeated by the same artist in the same year
+  validates :title, presence: true, uniqueness: { scope: :year, message: "can only happen once per year" }
+  validates :released, inclusion: { in: [true, false] }
+  # Optional if released is false
+  # Must not be blank if released is true
+  # Must be less than or equal to the current year
+  validates :year, numericality: { only_integer: true, less_than_or_equal_to: Time.now.year }
+  validates :artist_name, presence: true
+end
