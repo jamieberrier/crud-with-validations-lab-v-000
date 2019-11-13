d4071c2544b1a4b3f4d81d288b43d96b221544d1
jamieberrier <berrier.jamie@gmail.com>
2019-11-13 17:29:33 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,7 +5,7 @@ class Song < ApplicationRecord
   # [x] Optional if released is false
   # [ ] Must not be blank if released is true
   # [x] Must be less than or equal to the current year
-  validates :year, numericality: { only_integer: true, less_than_or_equal_to: Time.now.year }, presence: true, if: :released?
+  validates :year, presence: true, if: :released?, numericality: { only_integer: true, less_than_or_equal_to: Time.now.year }
   validates :artist_name, presence: true
 
   def released?
