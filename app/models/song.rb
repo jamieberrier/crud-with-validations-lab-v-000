350be8c1aa38c4f4700a47af24241debf9a00b94
jamieberrier <berrier.jamie@gmail.com>
2019-11-13 17:28:45 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,7 +3,7 @@ class Song < ApplicationRecord
   validates :title, presence: true, uniqueness: { scope: :year, message: "can only happen once per year" }
   validates :released, inclusion: { in: [true, false] }
   # [x] Optional if released is false
-  # Must not be blank if released is true
+  # [ ]Must not be blank if released is true
   # [x] Must be less than or equal to the current year
   validates :year, numericality: { only_integer: true, less_than_or_equal_to: Time.now.year }, presence: true, if: :released?
   validates :artist_name, presence: true
